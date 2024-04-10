4b3ad2431556c1485d6493b74c9e753fe6f70f57
thays pradoo <thayspradoo@hotmail.com>
2024-04-10 11:03:36 -0300

    mod

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,6 +1,10 @@
-var idade = '16'
-if (idade < 18) {
-    console.log('Menor de idade')
-} else {
-    console.log('Maior de idade')
+var idade = '22'
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18 || idade > 65) { 
+        console.log('Voto opcional')
+        
+} else if (idade >= 18) {
+    console.log('Voto obrigatório')
 }
---
+++ aula12/ex012.js
@@ -0,0 +1,16 @@
+var hora = '12'
+console.log(`Agora são extamente ${hora} horas.`)
+
+if (hora <= 5) {
+    console.log('Boa madrugada!')
+
+}else if (hora >= 6 && hora <= 12 ) {
+    console.log('Bom dia!')
+
+}else if (hora <= 18) {
+    console.log('Boa tarde!')
+
+}else if (hora > 18) {
+    console.log('Boa noite!')
+    
+}
