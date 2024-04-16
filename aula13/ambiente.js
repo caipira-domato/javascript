2cceed9fc08b4571f89ae10b526938e7ebce22f1
thays pradoo <thayspradoo@hotmail.com>
2024-04-16 18:50:13 -0300

    mod

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+ do {
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 500)
+
+
+
+
+
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
